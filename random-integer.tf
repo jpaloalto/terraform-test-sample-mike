7cb3b8397cc55d9d4aa115510621ffc5b7d033fa
jpaloalto <164952964+jpaloalto@users.noreply.github.com>
2024-04-03 15:27:27 -0700

    Create random-integer.tf

---
+++ random-integer.tf
@@ -0,0 +1,16 @@
+resource "random_integer" "priority" {
+  min = 1
+  max = 50000
+}
+
+
+resource "random_integer" "priority2" {
+  min = 1
+  max = 50000
+}
+
+
+resource "random_integer" "priority3" {
+  min = 1
+  max = 50000
+}
